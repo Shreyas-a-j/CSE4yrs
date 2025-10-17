ea9212c88d157bbd2d2c516d397594cc5417f10c
Shreyas Jadhav <airjadhav111@gmail.com>
2025-10-17 06:21:44 +0530

    new code added

--- pure skill not related to clg/DSA/data structures codes/quick_partition.c
+++ pure skill not related to clg/DSA/data structures codes/quick_partition.c
@@ -7,49 +7,53 @@ void swap(int* a, int* b) {
     *b = temp;
 }
 
-int partition(int arr[], int low, int high) {
-    int pivot = arr[high]; // pivot
-    int i = (low - 1); // Index of smaller element
+int partition(int arr[], int low, int high) {  // low = 2, high = 4;
+    int pivot = arr[high]; // pivot = 22;
+    int i = (low - 1); // i = 1;
+    for (int j = low; j <= high - 1; j++) { // j = 4  <= 3;
 
-    for (int j = low; j <= high - 1; j++) {
-        // If current element is smaller than or equal to pivot
         if (arr[j] <= pivot) {
-            i++; // increment index of smaller element
+            i++; // i = 2;
             swap(&arr[i], &arr[j]);
         }
     }
-    swap(&arr[i + 1], &arr[high]);
+    swap(&arr[i + 1], &arr[high]); 
     return (i + 1);
 }
 
+// {5, 34, 25, 12, 22, 11, 90, 64} 1st sort 
+// {5, 34, 25, 12, 22, 11, 64, 90} 2nd sort
+// {5, 11, 12, 22, 25, 34, 64, 90} 3rd step
+// {5, 11, 12, 22, 25, 34, 64, 90} 4th step final answer 
 
-void quickSort(int arr[], int low, int high) {
+void quickSort(int arr[], int low, int high) { // low = 2, high = 4;
     if (low < high) {
-        // pi is partitioning index, arr[pi] is now at right place
-        int pi = partition(arr, low, high);
-
-        // Recursively sort elements before partition and after partition
-        quickSort(arr, low, pi - 1);
-        quickSort(arr, pi + 1, high);
+        int pi = partition(arr, low, high); // low = 2, high = 4, pi = 3 
+        // smaller than 5 elements
+        quickSort(arr, low, pi - 1); // low = 2, high = 2;
+        // greater than 5 elements
+        quickSort(arr, pi + 1, high); // low = 4, high = 4;
     }
 }
 
-int main() {
-    int arr[] = {10, 7, 8, 9, 1, 5};
-    int n = sizeof(arr) / sizeof(arr[0]);
 
-    printf("Unsorted array: \n");
+
+int main() {
+    int arr[] = {64, 34, 25, 12, 22, 11, 90, 5}; 
+    int n = sizeof(arr) / sizeof(arr[0]); // 8;
+
+    printf("Unsorted array: \n"); 
     for (int i = 0; i < n; i++){
         printf("%d ", arr[i]);
     }
-    printf("\n");
+    printf("\n"); // {64, 34, 25, 12, 22, 11, 90, 5}
 
-    quickSort(arr, 0, n - 1);
+    quickSort(arr, 0, n - 1); // completed 
 
     printf("Sorted array: \n");
-    for (int i = 0; i < n; i++){
+    for (int i = 0; i < n; i++){ 
         printf("%d ", arr[i]);
     }
-    printf("\n");
+    printf("\n"); // {5, 11, 12, 22, 25, 34, 64, 90}
     return 0;
 }
